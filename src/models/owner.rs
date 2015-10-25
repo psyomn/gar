43598c2366154a2f3389bf90f5b8f340d6f7c193
psyomn <lethaljellybean@gmail.com>
2015-10-25 05:36:02 -0400

    models: add owner, and builder

---
+++ src/models/owner.rs
@@ -0,0 +1,62 @@
+pub struct Owner {
+    gh_id: u64,
+    nick: String,
+    email: String,
+}
+
+impl Owner {
+    fn new() -> Owner {
+        Owner {
+            gh_id: 0,
+            nick: "default".into(),
+            email: "default@default".into(),
+        }
+    }
+}
+
+pub struct OwnerBuilder {
+    owner: Owner,
+}
+
+/// Builder for the owner object (notice that Owner#new is private)
+impl OwnerBuilder {
+    pub fn new() -> OwnerBuilder {
+        OwnerBuilder {
+            owner: Owner::new()
+        }
+    }
+
+    pub fn gh_id(self, id: u64) -> OwnerBuilder {
+        OwnerBuilder {
+            owner: Owner {
+                gh_id: id,
+                nick: self.owner.nick,
+                email: self.owner.email,
+            }
+        }
+    }
+
+    pub fn nick(self, nick: &str) -> OwnerBuilder {
+        OwnerBuilder{
+            owner: Owner {
+                gh_id: self.owner.gh_id,
+                nick: nick.into(),
+                email: self.owner.email
+            }
+        }
+    }
+
+    pub fn email(self, email: &str) -> OwnerBuilder {
+        OwnerBuilder {
+            owner: Owner {
+                gh_id: self.owner.gh_id,
+                nick: self.owner.nick,
+                email: email.into(),
+            }
+        }
+    }
+
+    pub fn finalize(self) -> Owner {
+        self.owner
+    }
+}
