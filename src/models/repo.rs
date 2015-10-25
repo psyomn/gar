53e5e8ec1b1c84846c8ac11bb9de566471377ac1
psyomn <lethaljellybean@gmail.com>
2015-10-25 18:51:49 -0400

    models: add repobuilder

--- src/models/repo.rs
+++ src/models/repo.rs
@@ -8,3 +8,28 @@ pub struct Repo {
     owner: owner::Owner,
     url: String,
 }
+
+impl Repo {
+    pub fn new() -> Repo {
+        Repo {
+            gh_id: 0,
+            name: "default".into(),
+            language: "default".into(),
+            has_issues: false,
+            owner: owner::OwnerBuilder::new().finalize(),
+            url: "default".into(),
+        }
+    }
+}
+
+pub struct RepoBuilder {
+    repo: Repo,
+}
+
+impl RepoBuilder {
+    pub fn new() -> RepoBuilder {
+        RepoBuilder {
+            repo: Repo::new(),
+        }
+    }
+}
