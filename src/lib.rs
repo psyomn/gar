fd058bc23f5b700c93adb18887290c308cf0f192
psyomn <lethaljellybean@gmail.com>
2015-10-28 14:59:03 -0400

    lib: add minor documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,16 @@
 #![feature(path_ext)]
 
+//! This is a small tool that helps you interface with githubarchive.org
+//! It provides utilities to fetch specific archives, or fetch a range of archives via date.
+//!
+//! To fetch a particular archive you need to do the following:
+//!
+//!     gar -f 2013-11-11-11
+//!
+//! To fetch a range of archives you need to do the following:
+//!
+//!     gar --fetch-rng --from 2013-10-10-13 --to 2014-11-11-14
+//!
 extern crate rustc_serialize;
 extern crate time;
 
