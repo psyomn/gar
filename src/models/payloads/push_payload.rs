6d2bc468b4b1687698da628bed7e4e82aad8f662
psyomn <lethaljellybean@gmail.com>
2015-10-30 15:23:51 -0400

    payloads: possibly implemented sha_element payload

--- src/models/payloads/push_payload.rs
+++ src/models/payloads/push_payload.rs
@@ -11,6 +11,76 @@ pub struct PushPayload {
 
 impl PushPayload {
     pub fn from_json(json: &Option<&Json>) -> Option<PushPayload> {
+        if json.is_none() { return None }
+        if !json.unwrap().is_object() { return None }
+
+        let ref obj = json.unwrap().as_object().unwrap();
+
+        let head = match obj.get("head") {
+            Some(v) => match *v {
+                Json::String(ref s) => s.clone(),
+                _ => "".into(),
+            },
+            None => "".into(),
+        };
+
+        let refs = match obj.get("ref") {
+            Some(v) => match *v {
+                Json::String(ref s) => s.clone(),
+                _ => "".into(),
+            },
+            None => "".into(),
+        };
+
+        let none: &Vec<Json> = &vec![];
+        let size = match obj.get("size") {
+            Some(v) => match *v {
+                Json::U64(v) => v,
+                _ => 0,
+            },
+            None => 0,
+        };
+
+        let mut distinct = false;
+        let mut shas: Vec<ShaElement> = Vec::new();
+
+        // TODO not too happy about this.
+        // TODO this should be moved inside sha_element
+        match obj.get("shas") {
+            Some(v) => match v {
+                &Json::Array(ref a) => {
+                    for (ix, ref sha) in a.iter().enumerate() {
+                        let mut sha_str: String = String::new();
+                        let mut email_str: String = String::new();
+                        let mut desc_str: String = String::new();
+                        let mut user_str: String = String::new();
+
+                        match *sha {
+                             &Json::String(ref s) => match ix {
+                                 0 => sha_str   = s.clone(),
+                                 1 => email_str = s.clone(),
+                                 2 => desc_str  = s.clone(),
+                                 3 => user_str  = s.clone(),
+                                 _ => {},
+                             },
+                             &Json::Boolean(b) => distinct = b,
+                             _ => {},
+                        }
+
+                        shas.push(
+                            ShaElement::from_values(
+                                sha_str,
+                                email_str,
+                                desc_str,
+                                user_str,
+                                distinct));
+                    }
+                },
+                _ => {},
+            },
+            None => {},
+        }
+
         None
     }
 }
--- src/models/payloads/sha_element.rs
+++ src/models/payloads/sha_element.rs
@@ -13,4 +13,16 @@ impl ShaElement {
     pub fn from_json(json: &Option<Json>) -> Option<ShaElement> {
         None
     }
+
+    pub fn from_values(s: String, e: String, c: String, a: String,
+                       d: bool) -> ShaElement {
+        ShaElement {
+            sha: s,
+            email: e,
+            comment: c,
+            author: a,
+            distinct: d,
+        }
+    }
 }
+
