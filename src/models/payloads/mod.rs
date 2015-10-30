4907e8afae3bbc1c0e9899ed3d70dada28fe47c9
psyomn <lethaljellybean@gmail.com>
2015-10-30 03:03:44 -0400

    payloads: add watch event payload

--- src/models/payloads/mod.rs
+++ src/models/payloads/mod.rs
@@ -1,3 +1,6 @@
 pub mod issue_payload;
+pub mod watch_payload;
+
 pub use self::issue_payload::IssuePayload;
+pub use self::watch_payload::WatchPayload;
 
---
+++ src/models/payloads/watch_payload.rs
@@ -0,0 +1,33 @@
+use rustc_serialize::json::*;
+
+pub struct WatchPayload {
+    action: String,
+}
+
+impl WatchPayload {
+    pub fn new(s: String) -> WatchPayload {
+        WatchPayload {
+            action: s,
+        }
+    }
+
+    pub fn from_json(json: &Option<&Json>) -> Option<WatchPayload> {
+        if json.is_none() { return None }
+
+        let ref preobj = *json.unwrap();
+
+        if !preobj.is_object() { return None }
+
+        let json = preobj.as_object().unwrap();
+
+        let action: String = match json.get("action") {
+            Some(v) => match *v {
+                Json::String(ref s) => s.clone(),
+                _ => "".into(),
+            },
+            None => "".into(),
+        };
+
+        Some(WatchPayload { action: action } )
+    }
+}
