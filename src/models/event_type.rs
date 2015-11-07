56e4b96226704c52edd8b070a7fd1dd61f5d4fb7
psyomn <lethaljellybean@gmail.com>
2015-11-07 17:39:59 -0500

    event_type: add to_string trait for event_type

--- src/models/event_type.rs
+++ src/models/event_type.rs
@@ -30,3 +30,35 @@ pub enum EventType {
     Watch(Option<WatchPayload>),
 }
 
+impl ToString for EventType {
+    fn to_string(&self) -> String {
+        match *self {
+            EventType::Create => "CreateEvent".into(),
+            EventType::Fork => "ForkEvent".into(),
+            EventType::Other => "Other".into(),
+            EventType::CommitComment => "CommitCommentEvent".into(),
+            EventType::Delete(..) => "DeleteEvent".into(),
+            EventType::Deployment => "DeploymentEvent".into(),
+            EventType::DeploymentStatus => "DeploymentStatusEvent".into(),
+            EventType::Download => "DownloadEvent".into(),
+            EventType::Follow => "FollowEvent".into(),
+            EventType::ForkApply => "ForkApplyEvent".into(),
+            EventType::Gist => "GistEvent".into(),
+            EventType::Gollum(..) => "GollumEvent".into(),
+            EventType::IssueComment(..) => "IssueCommentEvent".into(),
+            EventType::Issues(..) => "IssueEvent".into(),
+            EventType::Member => "MemberEvent".into(),
+            EventType::Membership => "MembershipEvent".into(),
+            EventType::PageBuild => "PageBuildEvent".into(),
+            EventType::Public => "PublicEvent".into(),
+            EventType::PullRequest => "PullRequestEvent".into(),
+            EventType::PullRequestReviewComment => "PullRequestReviewCommentEvent".into(),
+            EventType::Push(..) => "PushEvent".into(),
+            EventType::Release => "ReleaseEvent".into(),
+            EventType::Repository => "RepositoryEvent".into(),
+            EventType::Status => "StatusEvent".into(),
+            EventType::TeamAdd => "TeamAddEvent".into(),
+            EventType::Watch(..) => "WatchEvent".into(),
+        }
+    }
+}
